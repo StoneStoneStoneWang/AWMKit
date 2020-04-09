4355cb8911c063faa54b62a98a7375f10777b836
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-10 01:31:06 +0800

    AWM

---
+++ Code/AWMConfig/AWMHandlerConfig.h
@@ -0,0 +1,226 @@
+//
+//  AWMHandlerConfig.h
+//  AWMContainer
+//
+//  Created by 王磊 on 2020/3/29.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import "AWMSignConfig.h"
+
+#if DEBUG
+
+#else
+
+#define AWMWelcomeCollectionViewCell uOvruKuUico
+
+#define AWMWelcomeViewController fQ3dtNqgqxpn
+
+#define AWMRootManager jrV9aCM7ao
+
+#define AWMNavigationConfigImpl wT4tWNoK3P
+
+#define RootManager j6Y0O5kvdW
+
+#define makeRoot nWzbB9zH1ovr
+
+#define awmSkipTap ozr0QZwmHYq
+
+#define rootShared Iyilj43MhPlz
+
+#define awmUnLogin Q6svrrobfsVX
+
+#define awmPresentLogin GUlkFIiejpxi
+
+#define awmRootViewController xrqsg44aCW
+
+#define awmPushReg iAzukq65IThV
+
+#define awmPushProtocol tIaDC1jkJ7
+
+#define awmPushFindPassword Slk8CVZEcXMH
+
+#define awmPushModifyPassword RQETC4jjBNp
+
+#define awmPushSetting g13QhXBkTvW
+
+#define awmPushUserInfo n_sF2BGBSP3
+
+#define awmPushName mnr59IPGqO
+
+#define awmPushSignature KcF7b981LY2r
+
+#define awmLogout _AHgrn8Hzv
+
+#define awmDismiss zrAzQu1ZW3zE
+
+#define awmPop FJMlE2D4ea
+
+#define awmTokenInvalid j7gUgKJdszel
+
+#define awmCheckLogin rf6W03glIsVh
+
+#define awmCreateProfile UlDQkrJGm3
+
+#define awmPushPrivacy aozYFdvsg_T6
+
+#define awmPushAbout eKWJZyXZQ8NY
+
+#define awmPushFeedBack P8XwqSSOZsE_
+
+#define awmCreateCarousel fFtlVAzCzAs
+
+#define awmPushReport NjCyk_iyObI
+
+#define AWMLoginBlock _BdpD_5sMx
+
+#define createLoginWithBlock nStoaGlBhx
+
+#define AWMLoginViewController Q1MjLkI9CZ
+
+#define AWMRegBlock IC3L09SGjRz
+
+#define AWMRegViewController piNyelKh6m9Y
+
+#define createRegWithBlock N7VDaDWK_qsz
+
+#define AWMProtocolViewController ztcWK9TPEok
+
+#define AWMFindPassworBlock iMwSc3Vu0hK
+
+#define AWMFindPasswordViewController fH5_JQ5jIH
+
+#define createPasswordWithBlock RFIT8TVBAN
+
+#define AWMModifyPasswordBlock qgjK7iQkno
+
+#define AWMModifyPasswordViewController ofXT1Pe_zpN
+
+#define AWMSettingTableViewCell SETG4dR3U_pk
+
+#define AWMSettingBlock pk7pnDPJ39i
+
+#define AWMSettingViewController IncIYaZdn7A7
+
+#define createSettingWithBlock xjmN4ioDsdZ
+
+#define AWMUserInfoActionType DlidWqgDpkfO
+
+#define AWMUserInfoBlock K6h683PcX_0
+
+#define AWMUserInfoViewController uOkJ5hUo_9
+
+#define AWMUserInfoTableViewCell JMN5ggVRfX7
+
+#define createUserInfoWithBlock gAto451MvP
+
+#define AWMFeedBackViewController xXS9Y9F6P6Pc
+
+#define AWMAboutViewController byOWOSxPUW6
+
+#define AWMAboutTableHeaderView Zjp6yBx7Kv3
+
+#define AWMAboutTableViewCell YXTYcodkRT
+
+#define AWMReportViewController XdCQhsOJmNu7
+
+#define AWMReportTableViewCell tsLGoLUbK5E0
+
+#define AWMReportBlock dw8ORJKGZ0ut
+
+#define AWMCarouselViewController vGRUVj2RYPtz
+
+#define AWMCarouselFormTwoLayout MGUh2EF3c_
+
+#define AWMCarouselFormOneLayout d4LkUL80ODVr
+
+#define AWMCarouselCollectionViewCell Tr8QaMUxZ83c
+
+#define AWMUserCenterCollectionViewCell O4O1YydiU6L
+
+#define AWMUserCentereBlock DNdmopXnUd
+
+#define AWMUserCenterViewController QStg1_q2c1B7
+
+#define createUserCenterWithBlock Wblg38vK9zAh
+
+
+
+#if AWMCONFIGURETYPEFLOWER
+
+#define awmPushOrder Iwt5DChopU0h
+
+#define awmPushAddress P8oljlwPI4Ur
+
+#define awmPushAddressSelected vceyxX36gLW9
+
+#define awmPushAddressEdit cVwgp1QZcd9D
+
+#define awmPushAddressEditAdd eYZaw4SU0WLp
+
+#define awmPushArea xY4aoQmfFu
+
+#define AWMAddressViewController xGVKhgrlVi1
+
+#define createAddressWithBlock bodtE4c9n6Tx
+
+#define AWMAddressBlock mCGoqUNNSUEp
+
+#define AWMAddressSelectedViewController b5RBykUDCQ
+
+#define AWMAddressSelectedBlock htLNRAGYHxn
+
+#define createAddressSelectedWithBlock g4fTWuVNwyK
+
+#define AWMAddressTableViewCell LrcUQh5MhdmR
+
+#define AWMAddressSelectedTableViewCell FZwqPnhs5E
+
+#define AWMAddressEditBlock lxjyg4TrqF
+
+#define AWMAddressEditActionType JzAvDH72L5e
+
+#define AWMAddressEditViewController tixJXJl7To
+
+#define creatAddressEditWithAddressBean MItX0XFLb8oK
+
+#define updateAddressEditArea CFfbBJrFpXKg
+
+#define AWMOrderViewController OhpooHxh1u
+
+#define AWMJoinUsViewController KmjXqClnht
+
+#define AWMNewsViewController fJ4xed1K3pO4
+
+#define AWMEpidemicViewController VGjPIcnlLY
+
+#define AWMEpidemicContentViewController ItoSdcSgYt2U
+
+#define AWMOfficialViewController ZGLR8fUYKw
+
+#define AWMOfficialContentViewController mySbtLg3qnM0
+
+#define AWMTrashViewController vVq7xN7CBO
+
+#define AWMKindViewController UE1E3Xq2NLW
+
+#define AWMSubCatagoryViewController N1rcIMJ7Yj
+
+#define AWMJoinersViewController WOr_UwAqqQi
+
+#define AWMHomeItemViewController f0Z3c9NMuaU
+
+#define AWMHomeItemHeaderView wtQkzh35VUJK
+
+#define AWMHomeItemCell ZDyQQzjY9r
+
+#define AWMTradeViewController T0uNnK3JtWq
+
+#define AWMHomeViewController J7EH3Zdhi8l
+
+
+#endif
+
+#endif
+
+
