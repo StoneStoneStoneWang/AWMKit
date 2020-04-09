e4d29bab4f60c9c287f086658392b8cdd22b94e1
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-09 21:44:45 +0800

    BBQ

---
+++ Framework/AWMOSS/AWMOSS.framework/Headers/AWMOSS.h
@@ -0,0 +1,19 @@
+//
+//  AWMOSS.h
+//  AWMOSS
+//
+//  Created by 王磊 on 2020/4/9.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWMOSS.
+FOUNDATION_EXPORT double AWMOSSVersionNumber;
+
+//! Project version string for AWMOSS.
+FOUNDATION_EXPORT const unsigned char AWMOSSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWMOSS/PublicHeader.h>
+
+#import <AWMOSS/AWMOSSManager.h>
---
+++ Framework/AWMReq/AWMReq.framework/Headers/AWMReq.h
@@ -0,0 +1,19 @@
+//
+//  AWMReq.h
+//  AWMReq
+//
+//  Created by 王磊 on 2020/4/9.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWMReq.
+FOUNDATION_EXPORT double AWMReqVersionNumber;
+
+//! Project version string for AWMReq.
+FOUNDATION_EXPORT const unsigned char AWMReqVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWMReq/PublicHeader.h>
+
+#import <AWMReq/AWMReqManager.h>
